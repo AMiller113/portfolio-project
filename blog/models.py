12111625731f29f458b693e14237feecef2301c8
Akeem Miller <ajayj46@gmail.com>
2020-02-20 19:37:11 -0500

    Set up template home page.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=150)
+    publication_date = models.DateTimeField()
+    blog_post_text = models.TextField()
+    blog_image = models.ImageField(upload_to='images/')
+
